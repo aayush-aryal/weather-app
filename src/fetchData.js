bcbc740c4171e529bec80862c1d34deef820c937
Aayush Aryal <rinnegan2024@gmail.com>
2022-11-17 18:41:30 +0545

    returns a object with necessary weather api attributes

--- src/fetchData.js
+++ src/fetchData.js
@@ -3,12 +3,24 @@
 export async function getWeatherData(userinput){
     userinput=userinput.toString()
     try {
-        let weatherAPIobject= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userinput}&APPID=0835900e4fbbbc9b5696bda5ee057129`,{mode:'cors'})
-        weatherAPIobject=await weatherAPIobject.json()
-        return weatherAPIobject
+        let response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userinput}&units=metric&APPID=0835900e4fbbbc9b5696bda5ee057129`,{mode:'cors'})
+        let responseweatherAPIobject=await response.json()
+        let filteredObject={temperature:
+            responseweatherAPIobject.main.temp,
+            name:responseweatherAPIobject.name ,
+            temp_min:responseweatherAPIobject.main.temp_min ,
+            temp_max:responseweatherAPIobject.main.temp_max,
+            main:responseweatherAPIobject.weather[0].main,
+            description:responseweatherAPIobject.weather[0].description,
+            icon:responseweatherAPIobject.weather[0].icon,
+            wind:responseweatherAPIobject.wind.speed
+        }
+        // get icon through http://openweathermap.org/img/w/10d.png
+        return filteredObject
 
-    }catch (error){
-        console.log('not found')
+    }catch (err){
+        console.error(err)
+       
     }
    
 }
