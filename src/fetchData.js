d284e8cc45be204e7dc949b496ebefba1b0d0d81
Aayush Aryal <rinnegan2024@gmail.com>
2022-11-17 12:14:17 +0545

    api call now handles error through try catch

--- src/fetchData.js
+++ src/fetchData.js
@@ -2,7 +2,13 @@
 
 export async function getWeatherData(userinput){
     userinput=userinput.toString()
-    let weatherAPIobject=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userinput}&APPID=0835900e4fbbbc9b5696bda5ee057129`)
-    weatherAPIobject= await weatherAPIobject.json()
-    console.log(weatherAPIobject)
+    try {
+        let weatherAPIobject= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userinput}&APPID=0835900e4fbbbc9b5696bda5ee057129`,{mode:'cors'})
+        weatherAPIobject=await weatherAPIobject.json()
+        return weatherAPIobject
+
+    }catch (error){
+        console.log('not found')
+    }
+   
 }
