9a0f5ed7023d57e4adfe43d273546695b7a7a240
Aayush Aryal <rinnegan2024@gmail.com>
2022-11-17 18:59:12 +0545

    makes api call on input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+import {getWeatherData} from './fetchData'
+
+
+const submitButton=document.querySelector('.submitButton')
+const locationInput=document.querySelector('.location-input')
+
+submitButton.addEventListener('click',(e)=>{
+   // prevent refresh
+   e.preventDefault()
+   // get location
+   const location=locationInput.value.toString()
+   // check if empty
+   if (location=='')return 
+   // else make api call
+   const locationData=getWeatherData(location)
+   // check if found 
+   locationData.then((LocationObj)=>{
+      console.log(LocationObj)
+   })
+})
+
+
